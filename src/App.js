f98d3e152e745ee4d161d6d25157f1b12840f4d9
Firmansyah Yanuar <fyfirman@gmail.com>
2020-05-28 16:04:37 +0700

    demo-rest-api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+
+  const [value, setValue] = useState({
+    users: [],
+    username: '',
+    password: ''
+  });
+
+  useEffect(() => {
+    // Update the document title using the browser API
+
+  });
+
+  const getUser = () => {
+    axios.get(`http://localhost:4000/api/user/`)
+      .then(res => {
+        setValue({
+          ...value,
+          users: res.data.User
+        });
+        console.log(JSON.stringify(value.users));
+      });
+  }
+
+  const handleChange = (event) => {
+    setValue({
+      ...value,
+      [event.target.name]: event.target.value
+    });
+  }
+
+  const handleSubmit = (event) => {
+    console.log(JSON.stringify(value));
+    event.preventDefault();
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={getUser}>Get List</button>
+        <ol>
+          {value.users.map((item, i) => <li key={i}>{item.username}</li>)}
+        </ol>
+        <div>
+        </div>
       </header>
     </div>
   );
