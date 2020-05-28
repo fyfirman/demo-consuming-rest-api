b19f888c5ecec0c5f7bae282e4cea27b70dd7d4e
Firmansyah Yanuar <fyfirman@gmail.com>
2020-05-28 16:21:57 +0700

    fetch-fuseki-api

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import qs from 'qs';
 import './App.css';
 
 function App() {
 
   const [value, setValue] = useState({
-    users: [],
-    username: '',
-    password: ''
+    courses: [],
   });
 
   useEffect(() => {
@@ -15,15 +14,49 @@ function App() {
 
   });
 
-  const getUser = () => {
-    axios.get(`http://localhost:4000/api/user/`)
-      .then(res => {
-        setValue({
-          ...value,
-          users: res.data.User
-        });
-        console.log(JSON.stringify(value.users));
+  const getData = async () => {
+    const BASE_URL = "http://localhost:3030/cari-kursus/query";
+
+    const headers = {
+      'Accept': 'application/sparql-results+json,*/*;q=0.9',
+      'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+    };
+
+    const queryData = {
+      query:
+        `PREFIX cl: <http://carikursus.com/ns/courselist#>
+  
+        SELECT ?name ?category ?platform ?link ?desc ?feature
+        WHERE
+        {
+          ?c     cl:name    ?name ;
+                cl:category    ?category ;
+                cl:platform    ?platform ;
+                cl:link    ?link ;
+                cl:desc    ?desc ;
+          OPTIONAL {?c     cl:feature  ?feature . }
+        }`
+    };
+
+    try {
+      const { data } = await axios(BASE_URL, {
+        method: 'POST',
+        headers,
+        data: qs.stringify(queryData)
       });
+      console.log(data);
+
+      // Convert Data
+      // const formatted_data = data.results.bindings.map((courses, index) => formatter(courses, index));
+      // console.log(formatted_data)
+
+      // setValue({
+      //   ...value,
+      //   courses: formatted_data
+      // });
+    } catch (err) {
+      console.error(err);
+    }
   }
 
   const handleChange = (event) => {
@@ -38,12 +71,24 @@ function App() {
     event.preventDefault();
   }
 
+  const formatter = (course, index) => {
+    return {
+      "id": index,
+      "name": course.name.value,
+      "category": course.category.value,
+      "platform": course.platform.value,
+      "link": course.link.value,
+      "desc": course.desc.value,
+      "feature": course.feature ? course.feature.value.split(", ") : ''
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <button onClick={getUser}>Get List</button>
+        <button onClick={getData}>Get List</button>
         <ol>
-          {value.users.map((item, i) => <li key={i}>{item.username}</li>)}
+          {/* {value.courses.map((item, i) => <li key={i}>{item.name}</li>)} */}
         </ol>
         <div>
         </div>
